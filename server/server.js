ea8d33e0f22c2eb467fa18ef1f953c7574eff0e3
allan raymond <raymonda916@gmail.com>
2025-08-14 18:17:39 +0300

    Update server.js: <describe changes>

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js'
+import competitionRoutes from './routes/competitionRoutes.js';
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/sixthman'
 
   //routes
   app.use('/api/auth', authRoutes)
+  app.use('/api/competitions', competitionRoutes)
 
 
 app.get('/api/test', (req, res) => {
