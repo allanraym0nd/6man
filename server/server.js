4cf97f8dd986628ee6794b3a737420fc1d862215
allan raymond <raymonda916@gmail.com>
2025-08-15 13:49:17 +0300

    Integrate prediction routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js'
 import competitionRoutes from './routes/competitionRoutes.js';
+import predictionRoutes from './routes/predictions.js';
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/sixthman'
   //routes
   app.use('/api/auth', authRoutes)
   app.use('/api/competitions', competitionRoutes)
+  app.use('api/predictions',predictionRoutes)
 
 
 app.get('/api/test', (req, res) => {
