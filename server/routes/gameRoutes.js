cc80fe86a1e5dbf812a3ff51694d2580c7b611a8
allan raymond <raymonda916@gmail.com>
2025-08-19 10:45:33 +0300

    gameRoutes.js

---
+++ server/routes/gameRoutes.js
@@ -0,0 +1,23 @@
+// import express from 'express';
+// import gameController from '../controllers/gameController.js';
+// import auth from '../middleware/auth.js';
+
+// const router = express.Router();
+
+// //public game routes
+
+// router.get('/today', gameController.getTodaysGames);
+// router.get('/schedule', gameController.getSchedule);
+// router.get('/:gameId', gameController.getGameById);
+// router.get('/live', gameController.getLiveGames);
+// router.get('/predictions-active', gameController.getPredictionActiveGames);
+
+// // admin/sync routes
+// router.post('/', auth, gameController.createOrUpdateGame);
+// router.put('/:gameId/result', auth, gameController.updateGameResult);
+
+
+
+
+
+// export default router;
