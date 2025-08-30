fbd58fddf1caee94a3ce6c3ae0834bbdfc776f38
allan raymond <raymonda916@gmail.com>
2025-08-30 12:46:46 +0300

    Update gameRoutes.js

--- server/routes/gameRoutes.js
+++ server/routes/gameRoutes.js
@@ -1,23 +1,24 @@
-// import express from 'express';
-// import gameController from '../controllers/gameController.js';
-// import auth from '../middleware/auth.js';
+import express from 'express';
+import gameController from '../controllers/gameController.js';
+import auth from '../middleware/auth.js';
+import game from '../models/game.js';
 
-// const router = express.Router();
+const router = express.Router();
 
-// //public game routes
+//public game routes
 
-// router.get('/today', gameController.getTodaysGames);
-// router.get('/schedule', gameController.getSchedule);
-// router.get('/:gameId', gameController.getGameById);
-// router.get('/live', gameController.getLiveGames);
-// router.get('/predictions-active', gameController.getPredictionActiveGames);
+router.get('/today', gameController.getTodaysGames);
+router.get('/schedule', gameController.getSchedule);
+router.get('/:gameId', gameController.getGameById);
+router.get('/live', gameController.getLiveGames);
+router.get('/predictions-active', gameController.getPredictionActiveGames);
 
-// // admin/sync routes
-// router.post('/', auth, gameController.createOrUpdateGame);
-// router.put('/:gameId/result', auth, gameController.updateGameResult);
-
-
-
+// admin/sync routes
+router.post('/', auth, gameController.createOrUpdateGame);
+router.put('/:gameId/result', auth, gameController.updateGameResult);
 
 
-// export default router;
+
+
+
+export default router;
