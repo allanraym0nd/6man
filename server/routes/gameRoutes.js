a0df8487aa2c45c04b23b3faa609ce50ebb2a4af
allan raymond <raymonda916@gmail.com>
2025-09-02 09:15:05 +0300

    Update game routes

--- server/routes/gameRoutes.js
+++ server/routes/gameRoutes.js
@@ -2,15 +2,16 @@ import express from 'express';
 import gameController from '../controllers/gameController.js';
 import auth from '../middleware/auth.js';
 import game from '../models/game.js';
+import { statsRateLimit } from '../middleware/rateLimiting.js';
 
 const router = express.Router();
 
 //public game routes
 
-router.get('/today', gameController.getTodaysGames);
-router.get('/schedule', gameController.getSchedule);
+router.get('/today', statsRateLimit ,gameController.getTodaysGames);
+router.get('/schedule', statsRateLimit, gameController.getSchedule);
 router.get('/:gameId', gameController.getGameById);
-router.get('/live', gameController.getLiveGames);
+router.get('/live', statsRateLimit, gameController.getLiveGames);
 router.get('/predictions-active', gameController.getPredictionActiveGames);
 
 // admin/sync routes
