82fed70a3413e53dc026429f1e0c584dab802c60
allan raymond <raymonda916@gmail.com>
2025-09-02 07:57:52 +0300

    Update auth routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,14 @@
 // routes/auth.js
 import express from 'express';
 import authController from '../controllers/authController.js';
+import validation from '../middleware/validation.js';
 
 const router = express.Router();
 
 // POST /api/auth/register
-router.post('/register', authController.register);
+router.post('/register', validation.validationRegistration, authController.register);
 
 // POST /api/auth/login
-router.post('/login', authController.login);
+router.post('/login', validation.validateLogin,authController.login);
 
 export default router;
