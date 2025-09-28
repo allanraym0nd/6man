b6631ddd35956225798ab5e47bb9c3947f885c39
allan raymond <raymonda916@gmail.com>
2025-09-28 17:27:50 +0300

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import ProtectedRoute from './components/auth/ProtectedRoute'
 import './styles/globals.css';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
+    console.log('App component rendering');
   return (
-    <>
+    
     <AuthProvider>
       <Router>
         <div className="App">
@@ -15,7 +17,7 @@ function App() {
             path="/"
             element={
               <ProtectedRoute>
-              {/* {} */}
+              <Dashboard/>
               </ProtectedRoute>  
             }
             />
@@ -34,7 +36,7 @@ function App() {
       </Router>
     </AuthProvider>
   
-    </>
+    
   )
 }
 
