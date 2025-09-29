443b08136723ccff942abe52006742d98dce0a68
allan raymond <raymonda916@gmail.com>
2025-09-29 19:33:27 +0300

    players.js

--- server/routes/players.js
+++ server/routes/players.js
@@ -8,11 +8,11 @@ const router = express.Router()
 // Public player routes
 router.get('/', playerController.getAllPlayers);
 router.get('/search/:name', playerController.searchPlayer)
-router.get('/:playerId', playerController.getPlayerById);
 router.get('/team/:teamId', playerController.getTeamRoster);
 router.get('/prediction-eligible', statsRateLimit, playerController.getPredictionEligiblePlayers);
 router.get('/:playerId/stats', playerController.getPlayerStats);
 router.get('/leaders/:stat', statsRateLimit, playerController.getStatLeaders);
+router.get('/:playerId', playerController.getPlayerById);
 
 // Admin/sync routes (protected)
 router.post('/', auth, playerController.createOrUpdatePlayer);
