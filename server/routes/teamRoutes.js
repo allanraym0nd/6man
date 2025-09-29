ad57b34e81750fe31243dc827422db875ab15896
allan raymond <raymonda916@gmail.com>
2025-09-29 19:33:27 +0300

    teamRoutes.js

--- server/routes/teamRoutes.js
+++ server/routes/teamRoutes.js
@@ -6,10 +6,10 @@ const router = express.Router()
 
 // Public team Routes
 router.get('/', teamController.getAllTeams);
-router.get('/:teamId', teamController.getTeamById);
 router.get('/standings/:conference', teamController.getStandings);
 router.get('/division/:division', teamController.getDivisionStandings);
 router.get('/:teamId/stats', teamController.getTeamStats);
+router.get('/:teamId', teamController.getTeamById);
 
 //Admin/sync routes
 router.post('/', auth, teamController.createOrUpdateTeam);
